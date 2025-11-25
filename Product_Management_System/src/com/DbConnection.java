51f0fafeeefdf34bab6b1b85ba57e081af4a8bb1
Jagdish <90018155+Jagdish-kamlekar@users.noreply.github.com>
2025-11-26 05:12:53 +0530

    Update DbConnection.java

--- Product_Management_System/src/com/DbConnection.java
+++ Product_Management_System/src/com/DbConnection.java
@@ -12,9 +12,13 @@ public class DbConnection
 		//System.out.println("Driver Loaded.......!!");
 		
 		Connection conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root","InSane");
+		//Connection conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/yourdbname","root/username","password"); 
+		//This is for Mysql Workbench
+		// if you want to do something else with it then you can do it for that, its driver URL and port will be different.
 		//System.out.println("COnnection Successfull....!\n");
 		
 		return conn;
 	}
 }
+
 
