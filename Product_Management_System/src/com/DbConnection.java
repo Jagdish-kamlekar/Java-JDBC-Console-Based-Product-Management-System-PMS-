4a48b8a2d2a9812cf70d0d960ee99c703226b276
Jagdish <90018155+Jagdish-kamlekar@users.noreply.github.com>
2025-11-26 05:08:48 +0530

    Update DbConnection.java

--- Product_Management_System/src/com/DbConnection.java
+++ Product_Management_System/src/com/DbConnection.java
@@ -9,11 +9,12 @@ public class DbConnection
 	public static Connection getConnection() throws ClassNotFoundException, SQLException
 	{
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		System.out.println("Driver Loaded.......!!");
+		//System.out.println("Driver Loaded.......!!");
 		
 		Connection conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root","InSane");
-		System.out.println("COnnection Successfull....!\n");
+		//System.out.println("COnnection Successfull....!\n");
 		
 		return conn;
 	}
 }
+
